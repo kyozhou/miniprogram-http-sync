79b6298dd12cb5cb0722a38f26ffdcdd25bf7e65
zhoubin <zhoubin@mirahome.me>
2019-01-21 16:32:59 +0800

    reform the code

--- http_sync.js
+++ http_sync.js
@@ -1,35 +1,40 @@
+/**
+ * date: 2019-01-21
+ * author: kyozhou1988@gmail.com
+ */
+
 import regeneratorRuntime from './regenerator-runtime'
 
 module.exports = {
-  httpRequestSync: async function (params) {
-    let _this = this;
-    let requestResult = await _this.httpRequestPromised(params)
-      .then((response) => {
-        return response;
-      });
-    return requestResult;
-  },
-  httpRequestPromised: function (params) {
-    if (params && params.url) {
-      let promise = new Promise((resolved, rejected) => {
-        wx.request({
-          url: params.url,
-          method: params.method | 'GET',
-          data: params.data | {},
-          header: params.header | {},
-          dataType: params.dataType | 'json',
-          responseType: params.responseType | 'text',
-          success: function (result) {
-            resolved(result);
-          },
-          fail: function (result) {
-            rejected(result)
-          }
-        });
-      });
-      return promise;
-    } else {
-      return false;
+    httpRequestSync: async function (params) {
+        let _this = this;
+        let requestResult = await _this.httpRequestPromised(params)
+            .then((response) => {
+                return response;
+            });
+        return requestResult;
+    },
+    httpRequestPromised: function (params) {
+        if (params && params.url) {
+            let promise = new Promise((resolved, rejected) => {
+                wx.request({
+                    url: params.url,
+                    method: params.method | 'GET',
+                    data: params.data | {},
+                    header: params.header | {},
+                    dataType: params.dataType | 'json',
+                    responseType: params.responseType | 'text',
+                    success: function (result) {
+                        resolved(result);
+                    },
+                    fail: function (result) {
+                        rejected(result)
+                    }
+                });
+            });
+            return promise;
+        } else {
+            return false;
+        }
     }
-  }
 };
